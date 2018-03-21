56a07066fbcca6f260f860f7b7b455a452183594
EGOISTK21 <egoistk21@gmail.com>
2018-03-21 17:14:36 +0800

    注入大概要使用到的第三方依赖

--- app/src/main/java/xyz/egoistk21/iFantasy/view/MainActivity.java
+++ app/src/main/java/xyz/egoistk21/iFantasy/view/MainActivity.java
@@ -1,7 +1,9 @@
-package xyz.egoistk21.iFantasy;
+package xyz.egoistk21.iFantasy.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import xyz.egoistk21.iFantasy.R;
 
 public class MainActivity extends AppCompatActivity {
 
